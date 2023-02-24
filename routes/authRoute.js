7b8a33cabeec9e2a6faeee99b961132f30b0d879
Fedor4994 <103943126+Fedor4994@users.noreply.github.com>
2023-02-24 20:25:26 +0200

    Change incorrect controller

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   loginController,
   registerController,
+  getCurrentUserController
 } from "../controllers/authControllers.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import {
@@ -13,6 +14,6 @@ const router = express.Router();
 
 router.post("/register", registerValidation, registerController);
 router.post("/login", loginValidation, loginController);
-router.get("/current", authMiddleware, loginController);
+router.get("/current", authMiddleware, getCurrentUserController);
 
 export default router;
