a982ed328707ae13f6cfebcb79c71fd782ece8f5
Fedor4994 <103943126+Fedor4994@users.noreply.github.com>
2023-03-12 16:03:31 +0200

    Add speed50 achievement

--- db/userModel.js
+++ db/userModel.js
@@ -32,6 +32,10 @@ const userSchema = new Schema(
         type: Boolean,
         default: false,
       },
+      speed50: {
+        type: Boolean,
+        default: false,
+      },
       speed70: {
         type: Boolean,
         default: false,
